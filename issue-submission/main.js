a47e23d694bafd211049b12aad8c55884a7bee35
habin-isa <habin@hotmail.co.uk>
2019-02-03 16:41:33 +0000

    refactors toggle of setting close

--- issue-submission/main.js
+++ issue-submission/main.js
@@ -1,57 +1,83 @@
-function fetchIssues() {
-	var issues = JSON.parse(localStorage.getItem('issues'));
-	var issuesList = document.getElementById('issuesList');
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
+function issuesList() {
+	return localStorage.getItem('issues') ?
+		JSON.parse(localStorage.getItem('issues')) :
+		[];
+};
+
+function fetchIssues() {
+	const issues = this.issuesList();
+	const issuesList = document.getElementById('issuesList');
+	
 	issuesList.innerHTML = '';
 
-	for (var i = 0; i < issues.length; i++) {
-		var id = issues[i].id;
-		var desc = issues[i].description;
-		var severity = issues[i].severity;
-		var assignedTo = issues[i].assignedTo;
-		var status = issues[i].status;
+	if (issues) {
+		issues.forEach(element => {
+			const {id, status, description, severity, assignedTo} = element;
 
-		issuesList.innerHTML += '<div class="well>' +
-														'<h6>Issue ID: ' + id + '</h6>' +
-														'<p><span class="label label-info">' +
-														status + '</span></p>' +
-														'<h3>' + desc + '</h3>' +
-														'<p>< span class="glyphicon glyphicon-user"></span>' +
-														assingedTo + '</p>' +
-														'<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> ' +
-														'<a href="#" class="btn btn-danger" onclick="deleteIssue"(\''+id+'\')">Delete</a>' +
-														'</div>';
+			issuesList.innerHTML += `<div class="well>
+			<h6>Issue ID: ${id} </h6>
+			<p><span class="label label-info">${status}</span></p>
+			<h3>${description}</h3>
+			<p><span class="glyphicon glyphicon-time"></span>${severity}
+			<span class="glyphicon glyphicon-user>${assignedTo}</p>
+			<a href="#" class="btn btn-warning" onclick="setStatusClosed('${id}')">Close</a>
+			<a href="#" class="btn btn-danger" onclick="deleteIssue('${id}')">Delete</a>
+			</div>`
+		});
 	}
 };
 
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
-
 function saveIssue(e) {
-	var issueId = chance.guid();
-	var issueDesc = document.getElementById('issueDescInput').nodeValue;
-	var issueSeverity = document.getElementById('issueSeverityInput').value;
-	var issueAssingedTo = document.getElementById('issueAssignedToInput').value;
-	var issueStatus = "Open";
+	const id = chance.guid();
+	const description = document.getElementById('issueDescInput').value || 'No Description Provided';
+	const severity = document.getElementById('issueSeverityInput').value;
+	const assignedTo = document.getElementById('issueAssignedToInput').value || 'No User Assigned';
+	const issues = JSON.parse(localStorage.getItem('issues')) || [];
 
-	var issue = {
-		id: issueId,
-		description: issueDesc,
-		severity: issueSeverity,
-		assignedTo: issueAssingedTo,
-		status: issueStatus
-	}
+	issues.push({
+		id,
+		description,
+		severity,
+		assignedTo,
+		status: 'Open'
+	});
 
-	if (localStorage.getItem('issues') === null) {
-		var issues = [];
-		issues.push(issue);
-		localStorage.setItem('issues', JSON.stringify(issues)); 
-	} else {
-		var issues = JSON.parse(localStorage.getItem('issues'));
-		issues.push(issue);
-		localStorage.setItem('issues', JSON.stringify('issues'));
-	};
-
+	localStorage.setItem('issues', JSON.stringify(issues))
 	document.getElementById('issueInputForm').reset();
 	fetchIssues();
 	e.preventDefault();
-}
+};
+
+function setStatusClosed(id) {
+	const issues = this.issuesList();
+	const updateIssue = issues.find(issueToClose => {
+		return issueToClose.id === id
+	})
+
+	const indOf = issues.indexOf(updateIssue)
+	issues.splice(indOf, 1)
+	issues.push({
+		id: updateIssue.id,
+		description: updateIssue.description,
+		severity: updateIssue.severity,
+		assignedTo: updateIssue.assignedTo,
+		status: 'Closed'
+	})
+
+	localStorage.setItem('issues', JSON.stringify(issues));
+	fetchIssues();
+};
+
+function deleteIssue(id) {
+	const issues = this.issuesList();
+	const issueToDelete = issues.find(issueToFind => {
+		return issueToFind.id === id
+	});
+
+	const indOf = issues.indexOf(issueToDelete)
+	issues.splice(indOf, 1)
+	localStorage.setItem('issues', JSON.stringify(issues));
+	fetchIssues();
+};
